442d0b5dcb4f3058a2c053473a792120bb856287
Chris O <o@Chriss-MacBook-Pro.local>
2019-05-30 02:57:08 -0500

    connected with server to respond to input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  let [userText,setText] = useState('');
+  let [reply, setReply] = useState('');
+
+  const getText = (input) =>{
+    setText(input.target.value);
+    
+
+  }
+  const onButton = () =>{
+    if(userText === '') return;
+    fetch('http://localhost:3001/ask', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          input: userText
+        })
+      })
+    .then(response=>response.json())
+    .then(answer => setReply(answer.output.text))
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = 'main'>
+        <h1> You're chatting with Chris </h1>
+        <h4> Enter your question or comment </h4>
+        <textarea onChange = {getText}className = 'input'/>
+        <br/>
+        <button onClick={onButton}>Enter</button>
+        <br/>
+      </div>
+      <div className = 'reply'>
+        {reply}
+      </div>
+
     </div>
   );
 }
